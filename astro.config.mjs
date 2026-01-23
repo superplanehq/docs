e2d45adaffd849e6dfc62ed877f7ec49ce103149
Igor Šarčević <igor@operately.com>
2026-01-23 13:47:15 +0100

    Flatten the install docs
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,25 +20,16 @@ export default defineConfig({
 				{
 					label: 'Installation',
 					items: [
+						{ label: 'Overview', slug: 'installation/overview' },
 						{ label: 'Try it on your computer', slug: 'installation/local' },
-						{
-							label: 'Single Host Installation',
-							items: [
-								{ label: 'EC2 on AWS', slug: 'installation/single-host/aws-ec2' },
-								{ label: 'Compute Engine on GCP', slug: 'installation/single-host/gcp-compute-engine' },
-								{ label: 'Hetzner', slug: 'installation/single-host/hetzner' },
-								{ label: 'DigitalOcean', slug: 'installation/single-host/digitalocean' },
-								{ label: 'Linode', slug: 'installation/single-host/linode' },
-								{ label: 'Generic server', slug: 'installation/single-host/generic-server' },
-							],
-						},
-						{
-							label: 'Kubernetes',
-							items: [
-								{ label: 'Google Kubernetes Engine', slug: 'installation/kubernetes/gke' },
-								{ label: 'Amazon Kubernetes (EKS)', slug: 'installation/kubernetes/amazon-eks' },
-							],
-						},
+						{ label: 'EC2 on AWS', slug: 'installation/single-host/aws-ec2' },
+						{ label: 'Compute Engine on GCP', slug: 'installation/single-host/gcp-compute-engine' },
+						{ label: 'Hetzner', slug: 'installation/single-host/hetzner' },
+						{ label: 'DigitalOcean', slug: 'installation/single-host/digitalocean' },
+						{ label: 'Linode', slug: 'installation/single-host/linode' },
+						{ label: 'Generic server', slug: 'installation/single-host/generic-server' },
+						{ label: 'Google Kubernetes Engine', slug: 'installation/kubernetes/gke' },
+						{ label: 'Amazon Kubernetes (EKS)', slug: 'installation/kubernetes/amazon-eks' },
 					],
 				},
         {
